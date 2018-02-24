d79de35e934ba56699cc004a85263b32b35e47ca
Juanjo <juanvillaescusa72@gmail.com>
2018-02-24 19:02:18 +0100

    Initial Commit

---
+++ goingReactive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goingReactive
+//
+//  Created by Juanjo García Villaescusa on 24/2/18.
+//  Copyright © 2018 Juanjo García Villaescusa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
